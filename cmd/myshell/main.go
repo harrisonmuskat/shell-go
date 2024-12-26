896674d988d435d6bd7f7cc6ff7d774146b8b009
Harrison Muskat <hmuskat@6river.com>
2024-12-26 11:31:22 -0500

    fix: handle exit code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,13 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
-
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	// Regular expression to match "exit X" where X is a number
+	exitPattern := regexp.MustCompile(`^exit (\d+)$`)
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -21,10 +24,22 @@ func main() {
 			os.Exit(1)
 		}
 
-		if command == "exit\n" {
-			break
+		// Trim the newline character from the command
+		command = strings.TrimSpace(command)
+
+		if matches := exitPattern.FindStringSubmatch(command); matches != nil {
+			// Convert the exit code to an integer
+			exitCode, err := strconv.Atoi(matches[1])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Invalid exit code:", matches[1])
+				continue
+			}
+
+			// Exit with the specified code
+			os.Exit(exitCode)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		// Handle all other commands
+		fmt.Println(command + ": command not found")
 	}
 }
